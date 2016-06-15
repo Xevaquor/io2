a2a7c58821b4bba57f84e50368510da8f8d4cd5c
Mateusz Bednarski <mateusz.bednarski@roche.com>
2016-06-15 19:18:32 +0200

    Jeszcze tylko przejrzeć ze spokojną głową

---
+++ IO2/IO2/ValueConverter/BoolToVisibilityConverter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+
+
+namespace IO2.ValueConverter
+{
+    class BoolToVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var b = (bool) value;
+            return b ? Visibility.Visible : Visibility.Hidden;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
